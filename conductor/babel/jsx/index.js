5c9189eaeb66e3bf191ae5de5083a4581ad8ba57
Layne Faler <laynefaler@gmail.com>
2019-08-29 08:41:04 -0700

    Updates to babel

--- conductor/babel/jsx/index.js
+++ conductor/babel/jsx/index.js
@@ -3,13 +3,15 @@ const path = require('path');
 
 
 module.exports = (settings) => () => {
-    const babelrc = JSON.parse(fs.readFileSync(path.join(settings.context, '.babelrc')))
+    const babelrc = JSON.parse(fs.readFileSync(path.join(settings.context, '.babelrc')));
     require('babel-register')(babelrc);
     const React = require('react');
     const { Provider } = require('react-redux');
     const { renderToStaticMarkup } = require('react-dom/server');
 
     return (Component, store) => renderToStaticMarkup(
-        React.createElement(Provider, { store }, React.createElement(Component))
+        <Provider store={store}>
+            <Component />
+        </Provider>
     );
 };
